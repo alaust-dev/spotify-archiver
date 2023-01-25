432acec6b095531e6f7909041e67dc31dcfeeef0
Alexander Aust <mail@alexander-aust.de>
2023-01-25 09:22:33 +0100

    Fix Jenkinsfile

--- tests/common/utils/date-util.test.ts
+++ tests/common/utils/date-util.test.ts
@@ -1,6 +1,7 @@
 import {expect, test} from "bun:test";
 import DateUtil from "../../../src/common/utils/date-util.js";
 
-test('01.04.2023 is KW 131', () => {
+test('01.04.2023 is KW 13', () => {
     expect(DateUtil.getWeekOfYear(new Date('2023-04-01'))).toBe(13)
 })
+
