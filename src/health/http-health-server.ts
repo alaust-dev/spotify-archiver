797c72f9586e66677ec8cc5c11816e17e1a396a8
Alexander Aust <mail@alexander-aust.de>
2023-01-26 23:29:42 +0100

    Add README and health endpoint

---
+++ src/health/http-health-server.ts
@@ -0,0 +1,29 @@
+import * as http from "http";
+import {log} from "../index.js";
+
+export interface HealthStatus {
+    lastRun: string
+}
+
+export class HttpHealthServer {
+
+    static healthStatus: HealthStatus = {
+        lastRun: new Date().toString()
+    }
+
+    start() {
+        http.createServer((req, res) => {
+            const lastRun = new Date(HttpHealthServer.healthStatus.lastRun)
+            const lastWeek = new Date()
+            lastWeek.setDate(lastWeek.getDate() -8)
+
+            if(lastRun < lastWeek ) {
+                res.writeHead(500)
+            }
+
+            res.write(JSON.stringify(HttpHealthServer.healthStatus))
+            res.end()
+        }).listen(30100)
+        log.info("Http health server started up!")
+    }
+}
